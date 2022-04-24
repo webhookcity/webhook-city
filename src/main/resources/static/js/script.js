ef01b8eda93ce3b42ae240e6ff1440ae335f9641
Sebastian Bayerl <sebastian.bayerl@enmacc.com>
2022-04-24 12:09:55 +0200

    initial minimal UI

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,31 +1,68 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    $('#refreshRequests').click(function() {
+        refreshRequests();
+    });
+
+    $("#deleteRequests").click(function() {
+        console.log("Delete requests");
+        deleteRequests();
+    });
+
+    $("#testGet").click(function() {
+        console.log("Test GET");
+        testGet();
+    });
+
+    $("#testPost").click(function() {
+        console.log("Test POST");
+        testPost();
+    });
+});
+
+function refreshRequests() {
     $.getJSON( "/api/v1/requests", function( data ) {
-       console.log("getJSON");
+        console.log("Get requests");
 
-       console.log(data);
+        console.log(data);
 
+        $("#eventList").empty();
         $.each( data, function( key, val ) {
             $("#eventList").append( "<li id='" + key + "'>" + val.url + "</li>" );
         });
     });
+}
 
-
-    $("#deleteEvents").click(function() {
-        console.log("Delete events.");
-
-        $.ajax({
-            url: '/api/v1/requests',
-            type: 'DELETE',
-            success: function(result) {
-                console.log("delete done");
-            }
-        });
+function deleteRequests() {
+    $.ajax({
+        url: '/api/v1/requests',
+        type: 'DELETE',
+        success: function(result) {
+            console.log("delete done");
+            refreshRequests();
+        }
     });
+}
 
-    $('#refreshPage').click(function() {
-        location.reload();
+function testGet() {
+    $.ajax({
+        url: '/catch/lala/test-get',
+        type: 'GET',
+        success: function(result) {
+            console.log("test GET done");
+            refreshRequests();
+        }
     });
+}
 
-});
+function testPost() {
+    $.ajax({
+        url: '/catch/lala/test-post',
+        type: 'GET',
+        success: function(result) {
+            console.log("test POST done");
+            refreshRequests();
+        }
+    });
+}
